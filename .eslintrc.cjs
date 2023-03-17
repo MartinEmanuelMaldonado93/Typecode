f0684b706c99baac27e2404d0e0e50a27bb998b4
Martin Emanuel <martinemanuel93@gmail.com>
2023-03-17 16:16:04 -0300

    add github auth provider

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    tsconfigRootDir: __dirname,
     project: "./tsconfig.json",
   },
   plugins: ["@typescript-eslint"],
